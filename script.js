7e64034356328375f1de19dd33e066d0290b97ce
Ello <157914147+Axellecz@users.noreply.github.com>
2025-05-03 16:42:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 fetch('pages.json')
   .then(res => res.json())
   .then(data => {
+    console.log("Image URLs:", data.pages);
     const container = document.getElementById("manga-container");
     container.innerHTML = "";
     data.pages.forEach(url => {
       const img = document.createElement("img");
       img.src = url;
       img.alt = "Manga page";
+      img.onerror = () => {
+        console.error("Failed to load:", url);
+      };
       container.appendChild(img);
     });
   })
